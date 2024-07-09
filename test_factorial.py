37836ee39c2c7a56463954f73b66287f85e57598
tabshilla maureen <adongtm@gmail.com>
2024-07-09 08:39:16 +0300

    all tests passed

---
+++ test_factorial.py
@@ -0,0 +1,28 @@
+from factorial import factorial
+
+def test_factorial_0():
+    assert factorial(0) == 1
+
+def test_factorial_1():
+    assert factorial(1) == 1
+
+def test_factorial_2():
+    assert factorial(2) == 2
+
+def test_factorial_3():
+    assert factorial(3) == 6
+
+def test_factorial_4():
+    assert factorial(4) == 24
+
+def test_factorial_5():
+    assert factorial(5) == 120
+
+
+
+
+
+
+
+
+
